62b87c28334cd0cd021b00e52911e9ad62644641
NielsJustesen <niels449@hotmail.com>
2017-02-17 13:02:10 +0100

    first commit

---
+++ Steampunk/Steampunk/Program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <irrKlang.h>
+using namespace std;
+
+
+using namespace irrklang;
+
+void main()
+{
+	ISoundEngine * engine = createIrrKlangDevice();
+
+	engine->play2D("bark.wav");
+	getchar();
+
+	engine->drop();
+
+}
