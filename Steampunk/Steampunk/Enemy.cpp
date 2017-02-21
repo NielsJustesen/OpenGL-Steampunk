5be0375e75c29e96fcfb863f204bbeeb958e2f58
claus jensen <clau3028@student.eadania.dk>
2017-02-21 12:17:14 +0100

    derp

--- Steampunk/Steampunk/Enemy.cpp
+++ Steampunk/Steampunk/Enemy.cpp
@@ -5,6 +5,11 @@
 
 Enemy::Enemy(int health)
 {
+	
+}
+
+void Enemy::Render()
+{
 	glTranslatef(1.4f, 0.0f, 0.0f);
 	glBegin(GL_QUADS); // Start drawing a quad primitive  
 	glVertex3f(-0.1f, -0.1f, 0.0f); // The bottom left corner  
@@ -12,6 +17,10 @@ Enemy::Enemy(int health)
 	glVertex3f(0.1f, 0.1f, 0.0f); // The top right corner  
 	glVertex3f(0.1f, -0.1f, 0.0f); // The bottom right corner  
 	glEnd();
+}
+
+void Enemy::Update()
+{
 }
 
 void Enemy::Attack(Player *target)
